41f3d39b1aaee6be184a2bfde50d3b326ad3ded2
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2021-11-18 09:14:36 +0100

    brownian check

--- src/com/lorenzoberti/session3/BrownianMotionDCheck.java
+++ src/com/lorenzoberti/session3/BrownianMotionDCheck.java
@@ -3,6 +3,7 @@
  */
 package com.lorenzoberti.session3;
 
+import net.finmath.stochastic.RandomVariable;
 import net.finmath.time.TimeDiscretization;
 import net.finmath.time.TimeDiscretizationFromArray;
 
@@ -32,33 +33,34 @@ public class BrownianMotionDCheck {
 		TimeDiscretization times = new TimeDiscretizationFromArray(initialTime, numberOfTimeSteps, deltaT);
 
 		// Constructor
-		// Write the constructor
+		BrownianMotionMultiD brownian = new BrownianMotionD(times, numberOfFactors, numberOfPaths);
 
-		// Check the average and the variance
-		System.out.println("Brownian average and variance test:");
+		// check the average and the variance
+		RandomVariable firstBrownian = brownian.getBrownianMotionAtSpecificTimeIndex(0, 10);
+		System.out.println("Average: " + firstBrownian.getAverage());
+		System.out.println("Variance: " + firstBrownian.getVariance());
 
-		// Write your code here
+		// check the covariance
+		RandomVariable secondBrownian = brownian.getBrownianMotionAtSpecificTimeIndex(0, 8);
+		System.out.println("Covariance:  " + secondBrownian.covariance(firstBrownian).getAverage());
 
-		// Check the covariance (between the same element of the brownian vector at
-		// different times)
-		System.out.println("Brownian covariance test:");
+		System.out.println();
 
-		// Write your code here
+		// check the brownian increment (they should be stationary and independent)
+		System.out.println("Brownian increment test:\t");
+		RandomVariable firstBrownianIncrement = brownian.getBrownianIncrement(3, 0);
+		System.out.println("Average of brownian increment: " + firstBrownianIncrement.getAverage());
+		System.out.println("Variance of brownian increment: " + firstBrownianIncrement.getVariance());
+		RandomVariable secondBrownianIncrement = brownian.getBrownianIncrement(4, 0);
+		System.out.println("Independence (covariance test): "
+				+ firstBrownianIncrement.covariance(secondBrownianIncrement).getAverage());
 
-
-
-		// Check the brownian increment (they should be stationary and independent)
-		System.out.println("Brownian increment test:");
-
-		// Write you code here
-
-
+		System.out.println();
 
 		// check the independence (Covariance test)
 		System.out.println("Independence between factors test:");
-
-		// Write your code here
-
+		RandomVariable secondFactor = brownian.getBrownianMotionAtSpecificTimeIndex(1, 5);
+		System.out.println("Independence (covariance test): " + secondFactor.covariance(firstBrownian).getAverage());
 
 	}
 
