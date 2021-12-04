13bb8f30c08c3d9cf1c1c248d37da52c416d93bd
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2021-12-04 20:26:54 +0100

    java doc

--- src/com/lorenzoberti/session6/AbstractEulerScheme.java
+++ src/com/lorenzoberti/session6/AbstractEulerScheme.java
@@ -106,7 +106,7 @@ public abstract class AbstractEulerScheme implements ProcessSimulation {
 		return path;
 	}
 
-	// This method generate the Euler scheme for a generical process
+	// This method generate the Euler scheme for a generic process
 	private void generate() {
 
 		// Write your code here
